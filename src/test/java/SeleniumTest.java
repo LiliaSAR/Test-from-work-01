8ca864dfff2797ac53c3082d1e61613520c7626a
Lilia Sargsyan <lilysargsyan1992@gmail.com>
2018-10-23 15:06:15 +0400

    test from work, first test with github

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -17,6 +19,25 @@ public class SeleniumTest {
         driver.get("https://www.google.com/");
         assertTrue(driver.getCurrentUrl().contains("https://www.google.com/"),"The page is not load corrency");
 
+    }
+
+    @Test
+    public void githubFailedLogin()
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\lilias\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://github.com/login");
+
+        WebElement loginField = driver.findElement(By.name("login"));
+        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement signInButton = driver.findElement(By.name("commit"));
+
+        loginField.sendKeys("mail@mail.com");
+        passwordField.sendKeys("pass01");
+
+        signInButton.click();
+        WebElement errorMessage = driver.findElement(By.cssSelector("#js-flash-container .flash-error"));
+        assertTrue(errorMessage.isDisplayed(), "Error message was not displayed!");
     }
 
     @AfterMethod
